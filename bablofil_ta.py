9782e10affd31634c12716cfd5661b11d20ad9c8
Bablofil <admin@bablofil.ru>
2019-06-19 11:29:47 +0000

    initial commit

---
+++ bablofil_ta.py
@@ -0,0 +1,140 @@
+import math
+
+def SMA(data, period):
+    if len(data) == 0:
+        raise Exception("Empty data")
+    if period <= 0:
+        raise Exception("Invalid period")
+
+    interm = 0
+    result = []
+    for i, v in enumerate(data):
+        if math.isnan(data[i]):
+            result.append(math.nan)
+            interm = 0
+        else:
+            interm += v
+            if (i+1) < period:
+                result.append(math.nan)
+            else:
+                result.append(interm/float(period))
+                if not math.isnan(data[i+1-period]):
+                    interm -= data[i+1-period]
+    return result
+
+def generalEMA(data, period, multiplier):
+    if period <= 1:
+        raise Exception("Invalid period")
+    
+    sma = SMA(data, period)
+    result = []
+    
+    for k, v in enumerate(sma):
+        if math.isnan(v):
+            result.append(math.nan)
+        else:
+            prev = result[k-1]
+            if math.isnan(prev):
+                result.append(v)
+                continue
+            ema = (data[k]-prev)*multiplier + prev
+            result.append(ema)
+    return result
+
+def EMA(data, period):
+    return generalEMA(data, period, 2/(float(period)+1.0))
+
+def EWMA(data, period):
+    return EMA(data, period)
+
+def SMMA(data, period):
+    return generalEMA(data, period, 1/(float(period)))
+
+def RMA(data, period):
+    return SMMA(data, period)
+
+def MMA(data, period):
+    return SMMA(data, period)
+
+def MACD(data, fastperiod, slowperiod, signalperiod):
+    macd, macdsignal, macdhist = [], [], []
+
+    fast_ema = EMA(data, fastperiod)
+    slow_ema = EMA(data, slowperiod)
+    
+    diff = []
+
+    for k, fast in enumerate(fast_ema):
+        if math.isnan(fast) or math.isnan(slow_ema[k]):
+            macd.append(math.nan)
+            macdsignal.append(math.nan)
+        else:
+            macd.append(fast-slow_ema[k])
+            diff.append(macd[k])
+
+    diff_ema = EMA(diff, signalperiod)
+    macdsignal = macdsignal + diff_ema
+
+    for k, ms in enumerate(macdsignal):
+        if math.isnan(ms) or math.isnan(macd[k]):
+            macdhist.append(math.nan)
+        else:
+            macdhist.append(macd[k] - macdsignal[k])
+
+    return macd, macdsignal, macdhist
+
+
+def RSI(data, period):
+    u_days = []
+    d_days = []
+
+    for i, _ in enumerate(data):
+        if i == 0:
+            u_days.append(0)
+            d_days.append(0)
+        else:
+            if data[i] > data[i-1] :
+                u_days.append(data[i] - data[i-1])
+                d_days.append(0)
+            elif data[i] < data[i-1]:
+                d_days.append(data[i-1] - data[i])
+                u_days.append(0)
+            else:
+                u_days.append(0)
+                d_days.append(0)
+
+    smma_u = SMMA(u_days, period)
+    smma_d = SMMA(d_days, period)
+
+    result = []
+
+    for k, _ in enumerate(data):
+        if smma_d[k] == 0:
+            result.append(100)
+        else:
+            result.append(100 - (100 / (1 + smma_u[k]/smma_d[k])))
+
+    return result
+
+def STOCH(high, low, closes, fastk_period, slowk_period, slowd_period):
+    fastk = []
+    
+    for i, _ in enumerate(closes):
+        if (i + 1) < fastk_period:
+            fastk.append(math.nan)
+        else:
+            lower_bound = i + 1 - fastk_period
+            upper_bound = i + 1
+            curr_low = min(low[lower_bound:upper_bound])
+            curr_high = max(high[lower_bound:upper_bound])
+            fastk.append(((closes[i] - curr_low) / (curr_high - curr_low)) * 100)
+       
+    fastk = EMA(fastk, slowk_period)
+    slowd = EMA(fastk, slowd_period)
+
+    return fastk, slowd
+
+def STOCHRSI(data, period, fastk_period, fastd_period):
+    rsi = RSI(data, period)
+    return STOCH(rsi, rsi, rsi, period, fastk_period, fastd_period)
+
